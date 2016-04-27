09ba52e076028a857105c0921553f9823ba082cd
karl <karlrivestharnois@hotmail.com>
2016-04-27 17:27:57 -0400

    AuthenticationManager

--- Appful/ViewController.swift
+++ Appful/ViewController.swift
@@ -1,80 +1,57 @@
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, AuthenticationDelegate {
+    
     @IBOutlet weak var login: UIButton!
     @IBOutlet weak var email: UITextField!
     @IBOutlet weak var password: UITextField!
-
-    var isAuthenticated = false {
-        didSet {
-            dispatch_sync(dispatch_get_main_queue()) { 
-                self.performSegueWithIdentifier("showAccount", sender: self)
-            }
-        }
-    }
     
-    var user: User? {
-        didSet {
-            ActivityIndicator.hide()
-            if user != nil { isAuthenticated = true }
-            else { isAuthenticated = false }
-        }
-    }
+    var authenticationManager: AuthenticationManager?
+    var user: User?
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print(user)
+        authenticationManager = AuthenticationManager(delegate: self)
     }
 
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        if let destination = segue.destinationViewController as? AccountController {
-            destination.user = user
-        }
-    }
-
-    override func shouldPerformSegueWithIdentifier(identifier: String, sender: AnyObject?) -> Bool {
-        return isAuthenticated
-    }
-    
-    private func authentication(email: String, _ password: String) {
-        fetchToken(email, password){ data, _, error in
-            if let err = error { print(err) }
-            if let jsonData = data{
-                if let authenticationToken = token(jsonData) {
-                    self.fetchUser(authenticationToken)
-                }
-            }
-        }
-    }
-    
-    private func fetchUser(token: String) {
-        fetchAccount(token) { data, _, error in
-            if let err = error { print(err) }
-            if let jsonData = data {
-                if let model = userModel(jsonData) {
-                    self.user = model
-                }
-            }
-        }
-    }
-    
     @IBAction func perform(sender: AnyObject) {
         ActivityIndicator.show()
         let email = self.email.text ?? ""
         let password = self.password.text ?? ""
-        self.authentication(email, password)
+        authenticationManager?.authentication(email, password)
+    }
+    
+    func authenticationResult(user: User?) {
+        ActivityIndicator.hide()
+        self.user = user
+        dispatch_sync(dispatch_get_main_queue()) {
+            self.performSegueWithIdentifier("showAccount", sender: self)
+        }
+    }
+    
+    override func shouldPerformSegueWithIdentifier(identifier: String, sender: AnyObject?) -> Bool {
+        return user != nil
+    }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if let destination = segue.destinationViewController as? AccountController {
+            destination.user = self.user
+        }
     }
 
     private func showAuthenticationError() {
-        let dismiss = UIAlertAction(title: "OK", style: .Default) { (UIAlertAction) in
+        let dismiss = UIAlertAction(title: "OK", style: .Default){ _ in
             self.dismissViewControllerAnimated(true, completion: nil)
         }
-
         let alert = UIAlertController(title: "Authentication Failed",
-                                      message: "Wrong email/password",
-                                      preferredStyle: .Alert)
-
+                                                      message: "Wrong email/password",
+                                                      preferredStyle: .Alert)
         alert.addAction(dismiss)
-
         presentViewController(alert, animated: true, completion: nil)
     }
 }
+
+
+
+
