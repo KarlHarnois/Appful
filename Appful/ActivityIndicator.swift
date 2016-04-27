e778475347359581ca278f74bc05c16f41079513
karl <karlrivestharnois@hotmail.com>
2016-04-27 16:56:40 -0400

    ActivityIndicator

--- Appful/ActivityIndicator.swift
+++ Appful/ActivityIndicator.swift
@@ -1,15 +1,18 @@
 //
-//  UIApplication+ActivityIndicator.swift
+//  ActivityIndicator.swift
 //  Appful
 //
 //  Created by Karl Rivest Harnois on 16-04-27.
 //  Copyright © 2016 Dynamo. All rights reserved.
 //
 
-import Foundation
+import UIKit
 
-extension UIApplication{
-    static func showActivityIndicator() {
+struct ActivityIndicator {
+    static func show() {
         UIApplication.sharedApplication().networkActivityIndicatorVisible = true
+    }
+    static func hide() {
+        UIApplication.sharedApplication().networkActivityIndicatorVisible = false
     }
 }
